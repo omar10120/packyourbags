6a12e6608bf96684388b5915ad3416c568a110f5
tamer tamer <amr.dawoodi@hotmail.com>
2025-04-21 16:46:56 +0300

    restore trip types

--- types/index.ts
+++ types/index.ts
@@ -1,23 +1,23 @@
-// export interface Trip {
-//   id: string
-//   departureCity: string
-//   destinationCity: string
-//   departureTime: string
-//   arrivalTime: string
-//   price: number
-//   availableSeats: number
-// }
+export interface Trip {
+  id: string
+  departureCity: string
+  destinationCity: string
+  departureTime: string
+  arrivalTime: string
+  price: number
+  availableSeats: number
+}
 
-// export interface Seat {
-//   id: string
-//   number: string
-//   isBooked: boolean
-//   isSelected?: boolean
-// }
+export interface Seat {
+  id: string
+  number: string
+  isBooked: boolean
+  isSelected?: boolean
+}
 
-// export interface CustomerInfo {
-//   name: string
-//   phone: string
-//   idNumber: string
-// }
+export interface CustomerInfo {
+  name: string
+  phone: string
+  idNumber: string
+}
 
